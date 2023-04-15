a66d900fa3c021b335f00b4eb3ea6c1ea845dce6
jaykim0413 <jaylego101@gmail.com>
2023-04-16 07:42:13 +0900

    I don't know why I'm keep on commiting

--- mayhem.java
+++ mayhem.java
@@ -10,6 +10,7 @@ public class mayhem {
     class6();
     class7();
     class8();
+    class9();
     System.out.println();
   }
 
@@ -274,7 +275,7 @@ public class mayhem {
     System.out.println("return\n: 헤더에 있는 리턴 타입과 동일한 타입을 가진 값을 return 해주어야 한다.\n");
     // Uses of Methods
     System.out.println("재귀 메소드가 아닌 경우,\nmain을 제외하고 단 하나의 메소드만 존재한다면 main 메소드만이 다른 메소드를 호출할 수 있다.\n");
-    System.out.println("메소드는 자신의 리턴 타입으로 쓰인다.");
+    System.out.println("메소드는 자신의 리턴 타입으로 쓰인다.\n");
     // Why do we use Methods?
     System.out.println(
         "반복되는 코드를 다시 작성하지 않아도 된다.\n: 프로그램이 논리적으로 간결해진다.\n: 우선 선언된 메소드는 이후 다시 사용할 수 있다.(재사용성)\n\n\"모듈화 Modularity\"");
@@ -283,6 +284,13 @@ public class mayhem {
   public static void class9() {
     // Class 9 : Method Overloading & API
     System.out.println("\n");
+
+    // Overloading
+    System.out.println(
+        "메소드 오버로딩: 같은 이름의 메소드를 다른 Signiture(시그니처)를 가지고 선언하는 것\n\n>> 시크니처 란? Method 고유의 정체성(?)과 같은 것(Ex. methodName, # of Arguments, Arguments의 타입)\n\n>> 메소드 오버로딩에서는 이름이 같은 메소드를 두 개 이상 만드는 것이니,\n- 메소드가 받는 Arguments(parameters)의 개수를 다르게 해주거나\n- 메소드가 받는 Arguments(parameters) 중 하나의 타입이라도 다르게 바꿔주거나\n- Arguments의 개수와 타입 모두를 바꿔주면 Method Overloading이 이루어진다.");
+    // API
+    System.out.println(
+        "API는 Application Programming Interface의 약자로 Programmer들이 많이 사용하는 Method를 모아둔 일종의 메소드 모음집이다.\n\nJAVA에서도 이런 API가 있는데, 이를 JAVA API Class라고 한다.\n- String과 같은 데이터 타입을 나타내는 Class(클래스)가 포함되어 있기도 하다.\n- 이외에도 입력을 받을 때 사용하는 Scanner 클래스 또한 java.util이라고 하는 Package 안에 있는 Scanner라는 클래스를 import 키워드를 통해서 불러오는 것으로 JAVA API Class에 속한다.\n\n이런 JAVA API에 있는 Class 혹은 Method는 import 키워드를 class 선언문 이전에 입력해서 사용할 수 있다.");
   }
 
   public static void class10() {
